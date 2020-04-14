a0f8e5db0687bea85811ab5467f0ee44a9fe1cdc
Timur <hts1238@mail.ru>
2020-04-14 16:21:46 +0500

    Auto-droped menu

--- static/scripts/menu.js
+++ static/scripts/menu.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.ajax({
+    /*$.ajax({
         type: "GET",
         url: "/api/category",
         data: {
@@ -53,12 +53,33 @@ $(function(){
                         }
                     }
                 }
-            }*/
+            }*\/
         },
         error: function(error) {
             console.error(error);
         },
         complete: function() {
         },
-    });
+    });*/
+
+    let fullpath = window.location.href.split("/");
+    fullpath = fullpath[fullpath.length-1];
+    let fpath = fullpath.split(".");
+
+    console.log(fpath);
+
+    let path = [fpath[0]];
+    for (let i = 1; i < fpath.length; i++) {
+        path.push(path[i-1] + "-" + fpath[i]);
+    }
+
+    console.log(path);
+
+    for (let i = 0; i < path.length; i++) {
+        $inp = $("#" + path[i]);
+        $inp.prop('checked', true);
+        $inp.prop('class', "here");
+    }
+
+    $($('.drop-end > a[href="/items/'+fullpath+'"]')[0].parentElement).prop('class', 'drop-end here');
 });
