1a7cccef62bad46f221a0f96b08f8fc3bc882762
Timur <hts1238@mail.ru>
2020-05-02 14:10:55 +0500

    Main page

--- main.py
+++ main.py
@@ -369,6 +369,10 @@ def item(path):
 
 
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+
 @app.route("/contacts")
 def contacts():
     return render_template("contacts.html")
